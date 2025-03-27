e395a6ea8ea166af4ed203bb9cae918b8893a25a
mmesbaah <mohannad.mesbaa7@gmail.com>
2025-03-27 04:56:15 +0200

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS Orders
 	order_id INT AUTO_INCREMENT PRIMARY KEY,
 	customer_id INT NOT NULL,
 	order_date DATE NOT NULL,
-	FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+	FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 ) ENGINE=InnoDB;
 CREATE TABLE IF NOT EXISTS Order_Details
 (
