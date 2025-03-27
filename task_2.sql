ee3af484fd12d5ea681ee36957cab2a6f2b5d4f1
mmesbaah <mohannad.mesbaa7@gmail.com>
2025-03-27 04:57:44 +0200

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -35,8 +35,7 @@ CREATE TABLE IF NOT EXISTS Order_Details
 	order_id INT NOT NULL,
 	book_id INT NOT NULL,
 	quantity DOUBLE NOT NULL,
-	FOREIGN KEY (order_id)
-REFERENCES ORDERS(order_id),
+	FOREIGN KEY (book_id) REFERENCES Books(book_id)
 	FOREIGN KEY (book_id)
 REFERENCES BOOKS(book_id)
 ) ENGINE=InnoDB;
